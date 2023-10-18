90253f93245e3063dd65554859891430a66cba67
dawitmanukian <davit.manukyan.d@tumo.org>
2023-10-19 01:29:09 +0400

    updates

--- src/components/cards-panel/CardsPanel.jsx
+++ src/components/cards-panel/CardsPanel.jsx
@@ -1,16 +1,41 @@
 import React from 'react'
 import ElementCard from '../element-card/ElementCard'
 import './cards-panel.css'
+import { useState } from 'react'
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 
 const CardsPanel = () => {
+
+  const [slideLeft, setSlideLeft] = useState(0)
+
+  if(slideLeft === 100 || slideLeft === -500){
+    setSlideLeft(0)
+  }
+
   return (
-    <div className='cards-panel'>
-        <ElementCard />
-        <ElementCard />
-        <ElementCard />
-        <ElementCard />
-        <ElementCard />
-        <ElementCard />
+    <div className='cards'>
+      <button onClick={() => setSlideLeft(slideLeft + 100)} className='arrow-btn'>
+        <FaAngleLeft />
+      </button>
+      <div className='slide-panel'>
+        <div className='slide' style={{left: `${slideLeft}%`}}>
+          <div className='lot-cards'>
+            <ElementCard />
+            <ElementCard />
+            <ElementCard />
+            <ElementCard />
+          </div>
+          <div className='lot-cards'>
+            <ElementCard />
+            <ElementCard />
+            <ElementCard />
+            <ElementCard />
+          </div>
+        </div>
+      </div>
+      <button onClick={() => setSlideLeft(slideLeft - 100)} className='arrow-btn'>
+        <FaAngleRight />
+      </button>
     </div>
   )
 }
