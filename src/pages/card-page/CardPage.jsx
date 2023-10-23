ad340d7d36180d267cabd3a328c717bbc87762a0
dawitmanukian <davit.manukyan.d@tumo.org>
2023-10-23 23:35:40 +0400

    updates

--- src/pages/card-page/CardPage.jsx
+++ src/pages/card-page/CardPage.jsx
@@ -11,10 +11,17 @@ const CardPage = () => {
   return (
     <div className='card-page'>
       <Link to={'/'}>
-        <button className='back-btn'><FaArrowLeftLong /></button>
+        <button className='back-btn' style={{zIndex: '2'}}><FaArrowLeftLong /></button>
       </Link>
       <div className='container'>
-        <ImagesSwiper images={['https://img.freepik.com/free-photo/old-white-house-garden_1150-12900.jpg?w=1380&t=st=1698086976~exp=1698087576~hmac=07fdbf402bb9c3e700804ff80c261f53ded82f85b4872641c12be875dbdb12b9','https://image.cnbcfm.com/api/v1/image/106758801-1603459526384-picture-perfect-beautiful-house-on-the-island-of-coronado-in-sunny-california-beautifully-landscaped_t20_6lJOrv.jpg?v=1603459593&w=1600&h=900']}/>
+        <div>
+          <ImagesSwiper images={['https://img.freepik.com/free-photo/old-white-house-garden_1150-12900.jpg?w=1380&t=st=1698086976~exp=1698087576~hmac=07fdbf402bb9c3e700804ff80c261f53ded82f85b4872641c12be875dbdb12b9','https://image.cnbcfm.com/api/v1/image/106758801-1603459526384-picture-perfect-beautiful-house-on-the-island-of-coronado-in-sunny-california-beautifully-landscaped_t20_6lJOrv.jpg?v=1603459593&w=1600&h=900']}/>
+        </div>
+        <div className='card-data'>
+          <h2>Կենտրոն, Զավարյան փող</h2>
+          <b className='address'>Զավարյան փող, Կենտրոն, Երևան</b>
+          <h3 className='price'>$ 105,000</h3>
+        </div>
       </div>
     </div>
   )
